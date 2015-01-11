70dc78dd9ac57fc9614686ce369f422b996dd67a
JoeCodex <joecodex10@gmail.com>
2015-01-11 01:02:49 -0500

    finish blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,20 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
   	@article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # #####################################################################
 
   def create
     @article = Article.new(article_params)
@@ -23,17 +32,6 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  def destroy
-    @article = Article.find(params[:id]).destroy
-    flash.notice = "Article '#{@article.title}' Deleted!"
-
-    redirect_to articles_path
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
@@ -42,4 +40,13 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.find(params[:id]).destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
+    redirect_to articles_path
+  end
+
 end
